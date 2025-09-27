8485ae8d1aec6a6c59269c8158314624ffb2cf99
julianab16 <julianamelis13@gmail.com>
2025-09-27 13:06:21 -0500

    uqdate controllers

--- api/dao/MoviesDAO.js
+++ api/dao/MoviesDAO.js
@@ -6,21 +6,6 @@ class MoviesDAO extends GlobalDAO {
     super(Movies);
   }
 
-  // Obtener todas las tareas de un usuario específico
-  async getByMoviesId(userId) {
-    return await this.model.find({ userId }).sort({ fecha: 1, hora: 1 });
-  }
-
-  // Obtener tareas por usuario y estado
-  async getByUserIdAndStatus(userId, estado) {
-    return await this.model.find({ userId, estado }).sort({ fecha: 1, hora: 1 });
-  }
-
-  // Verificar si una tarea pertenece a un usuario
-  async isTaskOwner(taskId, userId) {
-    const task = await this.model.findOne({ _id: taskId, userId });
-    return !!task;
-  }
 }
 
 module.exports = new MoviesDAO();
