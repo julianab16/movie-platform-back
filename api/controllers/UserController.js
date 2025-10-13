ffb21386d6024450a96f09a1276fedc43349979c
julianab16 <julianamelis13@gmail.com>
2025-10-12 20:58:08 -0500

    minimal changes

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,7 +1,6 @@
 const GlobalController = require("./GlobalController");
 const UserDAO = require("../dao/UserDAO");
 
-
 class UserController extends GlobalController {
   constructor() {
     super(UserDAO);
